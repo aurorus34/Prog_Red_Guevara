32713e31cfed9e281078ae2616bec7b00ea322a7
aurorus34 <alvaroguevaraet32@gmail.com>
2025-07-09 19:23:31 -0300

    Finalizada la primera parte de Java

---
+++ FlujosdeDatos/src/main/java/Datos/Main.java
@@ -0,0 +1,18 @@
+package Datos;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("Primer dato a mostrar.");
+		System.out.print("Hola.");
+		System.out.print("Chau.");
+		System.out.println("Segundo dato a mostrar");
+		System.out.write(77);
+		
+		byte[] aray = (60 , 45 , 100 , 59 , 77, -41 , 78) ;
+		System.out.write(aray);
+	}
+
+}
---
+++ FlujosdeDatos/src/main/java/classReader/Main.java
@@ -0,0 +1,9 @@
+package classReader;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		 
+	}
+	
+}
