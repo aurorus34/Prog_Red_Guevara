e30ee974f6ee9f560667b60a87562547470422d2
aurorus34 <alvaroguevaraet32@gmail.com>
2025-07-25 13:26:49 -0300

    Update Main.java

--- FlujosdeDatos/src/main/java/classReader/Main.java
+++ FlujosdeDatos/src/main/java/classReader/Main.java
@@ -1,9 +1,28 @@
 package classReader;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		 
+		
+		InputStreamReader is = new InputStreamReader( System.in );
+		BufferedReader buff = new BufferedReader( is );
+		
+		PrintStream ps = new PrintStream( System.out );
+		
+		try {
+			
+			String texto = buff.readLine();
+			ps.println( texto );
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
